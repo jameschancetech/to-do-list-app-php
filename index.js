9aafd15b3fa19c42d4442e23c74b72409c7e7e55
James Chance <38363488+jameschancetech@users.noreply.github.com>
2020-06-21 10:19:49 +0100

    Logic added

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'; // Import precompiled Bootstrap css
-import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
+
+const addToList = document.querySelector(`.addToList`);
+const noteContents = document.querySelector(`#note`);
+const noteArea = document.querySelector(`.notes-area`);
+
+function runDeleteButton() {
+  console.log(this);
+
+  // const deleteButton = document.querySelectorAll(`.delete`);
+  // deleteButton.addEventListener(`click`, e => {
+  //   e.srcElement.parentElement.remove();
+  // });
+}
+
+function addToContainer(e) {
+  const myNoteHTML = `
+    <div class="note-container">
+      <button class="delete"></button>
+      <h4>${noteContents.value}</h4>
+      <button class="favourite"></button>
+    </div>
+  `;
+  noteArea.innerHTML += myNoteHTML;
+  noteContents.value = ``;
+  runDeleteButton();
+}
+
+function checkContents() {
+  if (noteContents.value === ``) {
+    return;
+  }
+  addToContainer();
+}
+
+addToList.addEventListener(`click`, checkContents);
+
+document.addEventListener(`keydown`, e => {
+  if (e.key === `Enter`) {
+    checkContents();
+  }
+});
