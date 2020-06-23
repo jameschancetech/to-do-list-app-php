c088a22ae0209cf3ac47049ae39ecdcae02f2a97
James Chance <38363488+jameschancetech@users.noreply.github.com>
2020-06-23 17:09:44 +0100

    Background added

--- dist/to-do-list-app-js.e31bb0bc.js
+++ dist/to-do-list-app-js.e31bb0bc.js
@@ -194,7 +194,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./imgs/bg.jpg":[["bg.7077688b.jpg","imgs/bg.jpg"],"imgs/bg.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./node_modules/papercss/dist/paper.min.css");
@@ -269,7 +269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63081" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51881" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
